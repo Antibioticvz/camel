e820b5ceea196db85a1660170143930b5a36938a
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 02:16:25 +0400

    DropdownSelect component

---
+++ src/ui/DropdownSelect/DropdownSelect.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const DropdownSelect = ({ value, options, onChangeSelect }) => {
+    return (
+        <div className="select  is-rounded">
+            <select
+                value={value || options[0]}
+                onChange={e => onChangeSelect(e.target.value)}
+            >
+                <option>Select dropdown</option>
+                {options && options.map(option => <option key={option}>{option}</option>)}
+            </select>
+        </div>
+    )
+}
+
+DropdownSelect.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string),
+    onChangeSelect: PropTypes.func.isRequired,
+}
+DropdownSelect.defaultProps = {
+    value: null,
+    options: null,
+}
+
+export default DropdownSelect
