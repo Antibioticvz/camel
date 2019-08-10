1873171a4ebe9866e77951b96a92c5111b7c462b
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 00:59:50 +0400

    row component

---
+++ src/ui/RowWithHeader/RowWithHeader.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RowWithHeader = ({ header, rowComponent }) => {
+    return (
+        <div class="columns is-mobile">
+            <div class="column is-mobile">
+                <h2 className="subtitle is-2"> {header} </h2>
+            </div>
+            <div class="column">
+                {rowComponent}
+            </div>
+        </div>
+    )
+}
+
+RowWithHeader.propTypes = {
+    header: PropTypes.string.isRequired,
+    rowComponent: PropTypes.node,
+}
+RowWithHeader.defaultProps = {
+    rowComponent: null,
+}
+export default RowWithHeader
