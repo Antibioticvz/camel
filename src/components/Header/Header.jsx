f446cf666983db8702488f29d074768d31b74cfd
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 01:00:35 +0400

    fix title style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-const Header = () => <h1 className="title">How many camels for your boyfriend?</h1>
-
+const Header = () => <h1 className="title is-1">How many camels for your boyfriend?</h1>
 
 export default Header
--- src/screens/FormPage/FormPage.jsx
+++ src/screens/FormPage/FormPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import Row from 'ui/RowWithHeader'
+
 const FormPage = () => {
     return (
         <div>
             FormPage
+            <Row {...{
+                header: 'test',
+                rowComponent: 'rowComponent'
+            }} />
         </div>
     )
 }
