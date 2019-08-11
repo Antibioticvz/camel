f9f8c9170850523e37e65fc0d01f796836aa8597
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 04:13:42 +0400

    added onChange logic

--- src/ui/Slider/Slider.jsx
+++ src/ui/Slider/Slider.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Slider = ({ step, min, max, value }) => {
+const Slider = ({ step, min, max, value, onChangeSlider }) => {
     return (
-        <div class="columns is-mobile">
-            <div class="column is-one-fifth">
+        <div className="columns is-mobile">
+            <div className="column is-one-fifth">
                 <h2 className="subtitle is-3"> {value} </h2>
             </div>
-            <div class="column">
-                <input class="slider is-fullwidth is-info" step={step} min={min} max={max} value={value} type="range" />
+            <div className="column">
+                <input className="slider is-fullwidth is-info" type="range"
+                    step={step}
+                    min={min}
+                    max={max}
+                    value={value}
+                    onChange={e => onChangeSlider(e)}
+                />
             </div>
         </div>
     )
@@ -18,7 +24,8 @@ Slider.propTypes = {
     step: PropTypes.number.isRequired,
     min: PropTypes.number.isRequired,
     max: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
+    onChangeSlider: PropTypes.func.isRequired,
 }
 
 export default Slider
