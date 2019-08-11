9682d3bcfd18e63e08d8fcb1230140cbe4206522
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 05:31:50 +0400

    seted components with connected props and reducers

--- src/screens/FormPage/FormPage.jsx
+++ src/screens/FormPage/FormPage.jsx
@@ -1,67 +1,76 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { Link } from "react-router-dom"
 
+import * as ACTIONS from 'reducer/actions'
+import initialFormValues from 'reducer/initialFormValues'
+import formReducer from 'reducer/formReducer'
+import { HAIR_TYPE, EYE_TYPE, BEARD_TYPE, BODY_TYPE } from 'constants/form'
 import Row from 'ui/RowWithHeader'
 import DropdownSelect from 'ui/DropdownSelect'
 import RadioGroup from 'ui/RadioGroup'
 import Slider from 'ui/Slider'
 
 const FormPage = () => {
+    const [formValue, dispatch] = useReducer(formReducer, initialFormValues)
+
     return (
         <>
+            {formValue.cost}
             <Row {...{
                 header: 'Age',
                 rowComponent: <Slider {...{
-                    value: 22,
+                    value: formValue.age,
                     step: 1,
                     min: 14,
                     max: 70,
+                    onChangeSlider: e => dispatch({ type: ACTIONS.SET_AGE, value: e.currentTarget.value }),
                 }} />
             }} />
 
             <Row {...{
                 header: 'Height (cm)',
                 rowComponent: <Slider {...{
-                    value: 170,
+                    value: formValue.height,
                     step: 1,
                     min: 140,
                     max: 220,
+                    onChangeSlider: e => dispatch({ type: ACTIONS.SET_HEIGHT, value: e.currentTarget.value }),
                 }} />
             }} />
 
             <Row {...{
                 header: 'Haircolor',
                 rowComponent: <DropdownSelect {...{
-                    value: 'blond',
-                    options: ['blond', 'grey', 'red', 'black'],
-                    onChangeSelect: () => { },
+                    value: formValue.haircolor,
+                    options: Object.values(HAIR_TYPE),
+                    onChangeSelect: e => dispatch({ type: ACTIONS.SET_HAIRCOLOR, value: e.currentTarget.value }),
                 }} />
             }} />
 
             <Row {...{
                 header: 'Eyecolor',
                 rowComponent: <RadioGroup {...{
-                    value: 'gray',
-                    options: ['gray', 'blue', 'green'],
-                    onChangeRadio: () => { },
+                    value: formValue.eyecolor,
+                    options: Object.values(EYE_TYPE),
+                    onChangeRadio: e => dispatch({ type: ACTIONS.SET_EYECOLOR, value: e.currentTarget.name }),
                 }} />
             }} />
 
             <Row {...{
                 header: 'Beard',
                 rowComponent: <RadioGroup {...{
-                    value: '1',
-                    options: ['1', '2', '4'],
-                    onChangeRadio: () => { },
+                    value: formValue.beard,
+                    options: Object.values(BEARD_TYPE),
+                    onChangeRadio: e => dispatch({ type: ACTIONS.SET_BEARD, value: e.currentTarget.name }),
                 }} />
             }} />
 
             <Row {...{
                 header: 'Body',
                 rowComponent: <RadioGroup {...{
-                    value: '1',
-                    options: ['1', '2', '4'],
-                    onChangeRadio: () => { },
+                    value: formValue.body,
+                    options: Object.values(BODY_TYPE),
+                    onChangeRadio: e => dispatch({ type: ACTIONS.SET_BODY, value: e.currentTarget.name }),
                 }} />
             }} />
 
