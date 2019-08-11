d34d47fc8e2a1710a49e315ac435a7fb2d84edbe
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 04:32:58 +0400

    form constants

---
+++ src/constants/form.js
@@ -0,0 +1,24 @@
+export const HAIR_TYPE = {
+    BLOND: 'blond',
+    GREY: 'grey',
+    RED: 'red',
+    BLACK: 'black',
+}
+
+export const EYE_TYPE = {
+    GRAY: 'gray',
+    BLUE: 'blue',
+    GREEN: 'green',
+}
+
+export const BEARD_TYPE = {
+    NO: 'no',
+    SHORT: 'short',
+    LONG: 'long',
+}
+
+export const BODY_TYPE = {
+    FAT: 'fat',
+    SLIM: 'slim',
+    COOL: 'cool'
+}
