04ad4202e42757ca26b5873afe61bcee116650ea
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 04:41:56 +0400

    refactored with constants

--- src/reducer/initialFormValues.js
+++ src/reducer/initialFormValues.js
@@ -1,10 +1,12 @@
+import { HAIR_TYPE, EYE_TYPE, BEARD_TYPE, BODY_TYPE } from 'constants/form'
+
 export const initialFormValues = {
     age: '22',
     height: '170',
-    haircolor: 'blond',
-    eyecolor: 'gray',
-    beard: 1,
-    body: 1,
+    haircolor: HAIR_TYPE.BLOND,
+    eyecolor: EYE_TYPE.GRAY,
+    beard: BEARD_TYPE.NO,
+    body: BODY_TYPE.COOL,
 }
 
 export default initialFormValues
