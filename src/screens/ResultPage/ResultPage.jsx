df377b873a6c611a6686470652c37e4506222edc
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-08-11 07:25:16 +0400

    added fb share button

--- src/screens/ResultPage/ResultPage.jsx
+++ src/screens/ResultPage/ResultPage.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react'
 import { ReducerContext } from 'reducer/context'
 
+import Share from 'ui/FBShare'
+
 const ResultPage = () => {
     const { formValue } = useContext(ReducerContext)
     return (
         <>
             <h1 className="title is-1">{`Your boyfriend is worth ${formValue && formValue.cost} camels`}</h1>
+
+            <Share />
         </>
     )
 }
